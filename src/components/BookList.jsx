9ee1588dd97f8640e7fd8abae6353059750a5f7d
jayesh panchasara <panchasarajayesh2606@gmail.com>
2023-08-13 11:37:04 +0530

    first commit

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,30 +1,48 @@
 import { Button } from "@mui/material";
 import React from "react";
-import { useNavigate, NavLink } from "react-router-dom";
+import './Booklist.css'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const BookList = () => {
-    // const [data, setData] = useState([]);
+  
+const [books, setBooks] = useState([]);
 
-    
-   
-
-//   useEffect(() => {
-//     // Make an API call in the useEffect hook
-//     axios.get('https://book-e-sell-node-api.vercel.app/api/book/all')
-//       .then(response => {
-//         // Process the API response and set the data in state
-//         setData(response.data);
-//       })
-//       .catch(error => {
-//         console.error('Error fetching data:', error);
-//       });
-//   }, []);
+useEffect(() => {
+  axios.get('https://book-e-sell-node-api.vercel.app/api/book/all')
+    .then(response => {
+      setBooks(response.data.result);
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
+}, []);
 
     return (
         <div>
-            <h1>Data GridView</h1>
-            
-        </div>
+        <h1>Data GridView</h1>
+        <div id='containt-container'>
+                
+                    {books.map((book, index) => (
+                        <div key={index}>
+                            <div id="card-container">
+
+                                <div id="card">
+                                    <div><img id='card-image' src={book.base64image} alt='' /></div>
+                                    <div id="card-block">
+                                        <h4><b>{book.name}</b></h4>
+                                        <h4><b>Price :${book.price}</b></h4>
+                                        <h4><b>{book.category}</b></h4>
+                                        {/* <p>{book.description}</p> */}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+
+                </div>
+       
+      </div>
 
     );
 
