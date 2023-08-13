46b56670a99b3b81b16fe08056a0f13af6a0b62d
jayesh panchasara <panchasarajayesh2606@gmail.com>
2023-08-13 11:53:22 +0530

    final commit

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -5,46 +5,61 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const BookList = () => {
-  
-const [books, setBooks] = useState([]);
 
-useEffect(() => {
-  axios.get('https://book-e-sell-node-api.vercel.app/api/book/all')
-    .then(response => {
-      setBooks(response.data.result);
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error);
-    });
-}, []);
+  const [isLoading, setIsLoading] = useState(false);
+  const [books, setBooks] = useState([]);
 
-    return (
-        <div>
-        <h1>Data GridView</h1>
-        <div id='containt-container'>
-                
-                    {books.map((book, index) => (
-                        <div key={index}>
-                            <div id="card-container">
+  useEffect(() => {
+    setIsLoading(true);
 
-                                <div id="card">
-                                    <div><img id='card-image' src={book.base64image} alt='' /></div>
-                                    <div id="card-block">
-                                        <h4><b>{book.name}</b></h4>
-                                        <h4><b>Price :${book.price}</b></h4>
-                                        <h4><b>{book.category}</b></h4>
-                                        {/* <p>{book.description}</p> */}
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
+    setIsLoading(true);
+    axios.get('https://book-e-sell-node-api.vercel.app/api/book/all')
+      .then(response => {
+        setBooks(response.data.result);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        setIsLoading(false);
+        console.error('Error fetching data:', error);
+      });
+  }, []);
 
+  return (
+    <div>
+      <h1>All Book</h1>
+
+      {isLoading ? (
+        <center><h2><p>Loading...</p></h2></center>
+
+      ) : (
+        <ul><div id='containt-container'>
+
+          {books.map((book, index) => (
+            <div key={index}>
+              <div id="card-container">
+
+                <div id="card">
+                  <div><img id='card-image' src={book.base64image} alt='' /></div>
+                  <div id="card-block">
+                    <h4><b>{book.name}</b></h4>
+                    <h4><b>Price :${book.price}</b></h4>
+                    <h4><b>Category:{book.category}</b></h4>
+                   
+                  </div>
                 </div>
-       
-      </div>
+              </div>
+            </div>
+          ))}
 
-    );
+        </div>
+
+        </ul>
+      )}
+
+
+    </div>
+
+  );
 
 }
 
